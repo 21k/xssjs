d636214fe8b893738847c6da9125902c5713d987
21k <root@tmxk.org>
2017-11-18 01:31:50 +0800

    Update .js

--- .js
+++ .js
@@ -1,2 +1,2 @@
 alert(document.cookie)
-document.body.insertAdjacentHTML('afterbegin','<iframe style="border:0;position:fixed;width:100vw;height:100vh" src="//baidu.com"></iframe>')
+document.body.insertAdjacentHTML('afterbegin','<iframe style="border:0;background:#fff;z-index:10241024;position:fixed;width:100vw;height:100vh" src="//baidu.com"></iframe>')
